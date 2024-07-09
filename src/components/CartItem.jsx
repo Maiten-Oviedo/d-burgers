9116b28850e3556b21ba68ee8beb526ed96d075d
Maiten Oviedo <maitenoviedo513@gmail.com>
2024-07-09 15:43:48 -0300

    funcionalida del carrito

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const CartItem = ({item}) => {
+
+  let price = item.price * item.quantity;
   return (
     <article className='flex w-full justify-between items-center bg-slate-800'>
-      <img className=' w-24 h-auto' src={item.thumbnail} alt={item.name} />
-      <div className='w-full p-5 flex justify-between text-white'>
+      <img className='w-12 h-auto' src={item.thumbnail} alt={item.name} />
+      <div className='w-full p-5 flex justify-between text-white text-start'>
         <span className=''>{item.name}</span>
         <strong className=''>{item.quantity}</strong>
+        <strong className=''>${price}</strong>
       </div>
     </article>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,3 @@
-// src/components/Home.js
-import React from 'react';
-
 const Home = () => {
   return (
     <section id="home" className="bg-cover md:bg-center bg-no-repeat h-screen flex items-center w-screen"
@@ -11,8 +8,7 @@ const Home = () => {
           <span className="text-3xl md:text-5xl font-bold text-primary uppercase">hamburguesas?</span>
         </h1>
         <a href="#nuestra-carta" className='md:hidden bg-primary rounded-md text-white py-3 px-10'>VER CARTA</a>
-        <a href="#nosotros" className='md:hidden bg-primary rounded-md text-white py-3 px-10'>NOSOTROS</a>
-        <a href="https://maps.app.goo.gl/NKMDqkAn4DmsNvji6" target='_blank' rel='noopener noreferrer' className='md:hidden bg-primary rounded-md text-white py-3 px-10'>UBICACION</a>
+        <a href="https://maps.app.goo.gl/NKMDqkAn4DmsNvji6" target='_blank' rel='noopener noreferrer' className='md:hidden bg-primary rounded-md text-white py-3 px-10'>UBICACIÓN</a>
         <a href="https://maps.app.goo.gl/NKMDqkAn4DmsNvji6" target='_blank' rel='noopener noreferrer' className='bg-[#02a698] rounded-md text-white py-3 px-10 w-max md:px-4'>WHATSAPP</a>
       </div>
     </section>
